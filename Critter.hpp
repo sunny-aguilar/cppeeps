d1f9168b49d90e25a9b5da3c0b55ec43f989441c
Kuljot Biring <ksbiring1@gmail.com>
2019-01-30 13:16:05 -0800

    add critter class

---
+++ Critter.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Kuljot Singh Biring on 2019-01-30.
+//
+
+#ifndef GROUPPROJECT_CRITTER_HPP
+#define GROUPPROJECT_CRITTER_HPP
+
+
+class Critter
+{
+protected:
+
+public:
+    Critter();
+    virtual  ~Critter();
+    virtual move();
+    virtual breed();
+};
+
+
+#endif //GROUPPROJECT_CRITTER_HPP
