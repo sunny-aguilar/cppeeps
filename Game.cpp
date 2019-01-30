c6a4210eee98fef1a4762de359d9b36c2da0edde
Kuljot Biring <ksbiring1@gmail.com>
2019-01-30 13:01:27 -0800

    updated Menu class, added Game class, updated main.cpp

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kuljot Singh Biring on 2019-01-30.
+//
+
+#include "Game.hpp"
---
+++ Game.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Kuljot Singh Biring on 2019-01-30.
+//
+
+#ifndef GROUPPROJECT_GAME_HPP
+#define GROUPPROJECT_GAME_HPP
+
+
+class Game {
+
+};
+
+
+#endif //GROUPPROJECT_GAME_HPP
