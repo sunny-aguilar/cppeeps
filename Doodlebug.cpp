89d4d2b37985dfb511fea2580de1bae189c925b7
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-31 11:17:23 -0800

    update comments

--- Doodlebug.cpp
+++ Doodlebug.cpp
@@ -1,5 +1,5 @@
 /*********************************************************************
-** Program name:    Critter.hpp
+** Program name:    Doodlebug.hpp
 ** Author:          Kuljot Biring, Rachel Schlick, Ryan Gross,
 **                  Sandro Aguilar, Jeesoo Ryoo
 ** Date:            02/17/2019
