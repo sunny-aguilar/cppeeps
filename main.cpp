4b70b1fa699b48135ee3e986a662b61db000d52c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-31 09:43:17 -0800

    update comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************
 ** Program name: 		main.cpp
-** Author:      Kuljot Biring, Rachel Schlick, Ryan Gross, Sandro Aguilar, Jeesoo Ryoo
+** Author:          Kuljot Biring, Rachel Schlick, Ryan Gross, Sandro Aguilar, Jeesoo Ryoo
 ** Date: 				02/17/2019
 ** Description: 		Util functions can be used in multiple places
 *********************************************************************/
