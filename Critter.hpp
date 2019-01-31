8cffbf817ad15bb598c776b0063c3bda0661c07b
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-31 11:13:48 -0800

    update comments

--- Critter.hpp
+++ Critter.hpp
@@ -1,7 +1,7 @@
 /*********************************************************************
 ** Program name:Critter.hpp
 ** Author:Kuljot Biring, Rachel Schlick, Ryan Gross,
-** 						Sandro Aguilar, Jeesoo Ryoo
+**Sandro Aguilar, Jeesoo Ryoo
 ** Date:02/17/2019
 ** Description:Critter class description here
 *********************************************************************/
