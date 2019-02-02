51b60b16646d91128e40c39f267f3f2a84c638aa
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-02-01 16:42:20 -0800

    update move ant

--- Game.cpp
+++ Game.cpp
@@ -287,6 +287,13 @@ void Game::eatAnts() {
 *********************************************************************/
 void Game::moveDoodlebugs() {
     cout << "Moving doodlebugs" << endl;
+    for (int r_index = 0; r_index < row; r_index++) {
+        for (int c_index = 0; c_index < col; c_index++) {
+            if (board[r_index][c_index]->getCritterType() == "X") {
+                board[r_index][c_index]->move();
+            }
+        }
+    }
 }
 
 /*********************************************************************
