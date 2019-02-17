51d1a1b18ae007833a6ced25b490c5397e115ef5
Rachel Schlick <45744803+schlick-student@users.noreply.github.com>
2019-02-17 11:27:26 -0800

    main.cpp comment block

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 /*********************************************************************
+** Program Name:    Predator vs Prey
 ** Author:          Kuljot Biring, Rachel Schlick, Ryan Gross,
 **                  Sandro Aguilar, Jeesoo Ryoo
 ** Date:            02/17/2019
 ** Description:     Runs instance of "predator-prey" game
-** Note: Run make, then ./predator-prey
+** Note:            run "make", then "make run"
 *********************************************************************/
 #include <iostream>
 #include "Game.hpp"
